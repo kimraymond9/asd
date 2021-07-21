f39465a5776db8f658d3f7a8c5c86cbc8ed871df
Raymond <kimraymond9@gmail.com>
2021-07-21 21:55:53 +1200

    123

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('document').ready(function () {
+    var uuid = uuidv4();
+    if (localStorage.getItem("uuidv4") === null) {
+        localStorage.setItem("uuidv4", uuid);
+    } else {
+
+    }
+});
